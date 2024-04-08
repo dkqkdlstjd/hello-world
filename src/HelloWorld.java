5eb1044b6f32f3f6424ba5032af045e4cd001adc
dkqkdlstjd <dkqkdlstjd@naver.com>
2024-04-08 21:58:28 +0900

    hi world 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@
 public class HelloWorld {
 	public static void main(String[]args) {
 		System.out.println("helloworld");
+		System.out.println("Hi world");
+		System.out.println("Hi world2");
 	}
 }
